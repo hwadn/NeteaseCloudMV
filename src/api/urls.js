6ed65865a2f227310391d49f600ae406382dabf7
Huadong <43377087+IamHuadong@users.noreply.github.com>
2019-12-22 10:10:34 +0800

    Update urls.js

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,7 +1,7 @@
 const urls = {
 	selfUrl: "http://localhost:8080",
 	nodeUrl: "http://localhost:3000",
-	ipCityUrl: "http://hn216.api.yesapi.cn/?s=Ext.IP.GetInfo&app_key=65C1298539FD9D8D9E6C2908F1AC7231",
-	cityWeatherUrl: "http://hn216.api.yesapi.cn/?s=App.Common_Weather.LiveWeather&app_key=65C1298539FD9D8D9E6C2908F1AC7231&city=",
+	ipCityUrl: "http://hn216.api.yesapi.cn/?s=Ext.IP.GetInfo&app_key=xxxxxxxxxxx", // 使用参考小白开放接口
+	cityWeatherUrl: "http://hn216.api.yesapi.cn/?s=App.Common_Weather.LiveWeather&app_key=xxxxxx&city=",// 使用参考小白开放接口
 }
 export default urls;
